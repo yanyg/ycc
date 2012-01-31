438808dfc78ff855d1a7035f73aa5a9f31ad6b52
yanyonggang <yanyonggang@wisdata.com.cn>
2012-01-31 11:09:19 +0800

    test rbtree

--- src/tests/algos/test-rbtree.c
+++ src/tests/algos/test-rbtree.c
@@ -1,53 +1,99 @@
-/*
- * test-rbtree.c -- Test suits of Red Black Trees
- *
- * Copyright (C) 2012-2013 yanyg (cppgp@qq.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING, if not see
- * <http://www.gnu.org/licenses/>.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <ycc/debug.h>
+#include <time.h>
 
 #include <ycc/algos/rbtree.h>
 
-static int test_rbtree(size_t size);
+struct node {
+	int val;
+	struct rb_node rb_node;
+};
 
-int main(int argc, char **argv)
+static int node_cnt = 0;
+
+struct node* node_alloc(int val)
 {
-	int rtn;
+	struct node *p = malloc(sizeof(*p));
 
-	if (1 == argc)
-		return test_rbtree(1024*1024);
-	else {
-		int i;
-		for(i = 1; i < argc; ++i) {
-			size_t size = (size_t)strtoul(argv[i], NULL, 10);
-
-			rtn = test_rbtree(size);
-			if (rtn)
-				return rtn;
-		}
+	if (p) {
+		p->val = val;
+		++node_cnt;
 	}
 
-	return 0;
+	return p;
 }
 
-static int test_rbtree(size_t size)
+void node_free(struct node *p)
 {
+	free(p);
+	if(p)--node_cnt;
+}
+
+static inline int compare(const struct rb_node *rb_node, const void *arg)
+{
+	struct node *p = rb_entry(rb_node, struct node, rb_node);
+
+	return p->val - *(int*)arg;
+}
+
+static int compare_link(const struct rb_node *rb_node1,
+			const struct rb_node *rb_node2,
+			const void *arg)
+{
+	struct node *p1 = rb_entry(rb_node1, struct node, rb_node);
+	struct node *p2 = rb_entry(rb_node2, struct node, rb_node);
+	return p1->val - p2->val;
+}
+
+static void destroy(struct rb_node *node, const void *arg)
+{
+	node_free(rb_entry(node, struct node, rb_node));
+}
+
+int main()
+{
+	int i, val;
+	struct node *p;
+	struct rb_node *rb_node;
+
+	RB_DECLARE(rb);
+
+	srand( (unsigned int)time(NULL) );
+
+	for (i = 0; i < 1024*1024; ++i) {
+		p = node_alloc(rand()%100 + 100);
+		rb_insert(&p->rb_node, &rb, compare_link, NULL);
+	}
+
+#ifndef NDEBUG
+	printf("depth: {%zu, %zu}\n", rb_depth_min(&rb), rb_depth_max(&rb));
+#endif
+
+	rb_node = rb_first(&rb);
+	if (rb_node) {
+		val = rb_entry(rb_node, struct node, rb_node)->val;
+		rb_node = rb_next(rb_node);
+
+		while (rb_node) {
+			register int val_new = rb_entry(rb_node, struct node, rb_node)->val;
+			if (val > val_new) {
+				printf("error: %d, %d\n", val, val_new);
+			}
+			rb_node = rb_next(rb_node);
+		}
+	}
+	rb_node = rb_first(&rb);
+	printf("1 node_cnt: %d\n", node_cnt);
+	i = 105;
+	rb_erase_equal(&rb, compare, destroy, &i, NULL);
+	printf("2 node_cnt: %d\n", node_cnt);
+	rb_clear(&rb, destroy, NULL);
+	printf("3 node_cnt: %d\n", node_cnt);
+	rb_node = rb_first(&rb);
+	if (rb_node) {
+		printf("error: rb_node should be null\n");
+	}
+
+
 	return 0;
 }
