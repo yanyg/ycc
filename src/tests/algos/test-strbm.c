671ecc92190939b896bf6f9a31b9b01be6ae11c8
yanyg <yygcode@163.com>
2012-02-09 01:47:46 +0800

    test-suits: test-bm.c

--- src/tests/algos/test-strbm.c
+++ src/tests/algos/test-strbm.c
@@ -9,29 +9,24 @@
 
 int main()
 {
-	char *haystack = "I'm yanyg, i'm from china, yyg is a good boy";
-	char *needle = "hina, yy";
+	char needle[32];
+	const char *haystack = "I'm yanyg, i'm from china, yyg is a good boy~!";
 	size_t table_sgs[64], table_ebc[UCHAR_MAX+1];
 
-	strbm_init(needle, strlen(needle), table_sgs, table_ebc);
-
 	int i;
-	for (i = 0; i < strlen(needle); ++i)
-		printf("sgs[%d] = %zu\n", i, table_sgs[i]);
+	for (i = 0; i < strlen(haystack); ++i) {
+		strncpy(needle, haystack+i, 16);
+		needle[16] = '\0';
+		strbm_init(needle, strlen(needle), table_sgs, table_ebc);
+		char *result = strbm_find(haystack, needle,
+					  strlen(haystack), strlen(needle),
+					  table_sgs, table_ebc);
+		if (result)
+			printf("find success: %s\n", result);
+		else
+			printf("find failed !\n");
 
-	char *result = strbm_find(haystack, needle,
-				  strlen(haystack), strlen(needle),
-				  table_sgs, table_ebc);
-	//strbmh_init(needle, strlen(needle), table_ebc);
-
-	//char *result = strbmh_find(haystack, needle,
-	//			  strlen(haystack), strlen(needle),
-	//			  table_ebc);
-
-	if (result)
-		printf("find success: %s\n", result);
-	else
-		printf("find failed !\n");
+	}
 
 	return 0;
 }
